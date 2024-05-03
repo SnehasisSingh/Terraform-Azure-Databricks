9d1cb2002d4150c946551378b54cc01794f88636
Snehasis Prasad Singh <167405879+SnehasisSingh@users.noreply.github.com>
2024-05-03 23:55:47 +0530

    Create&Update Cluster

--- Cluster/Create_Cluster/Main.tf
+++ Cluster/Create_Cluster/Main.tf
@@ -1,1 +1,27 @@
-hello
+provider "your_provider" {
+  # Define your provider configuration here
+}
+
+resource "your_job_cluster_resource" "example_cluster" {
+  # Define your job cluster resource configuration here
+  # Include the necessary parameters like cluster size, instance types, software versions, etc.
+
+  # Example parameters:
+  cluster_name = "example-cluster"
+  instance_type = "m5.large"
+  cluster_size = 5
+  software_version = "v2.0"
+}
+
+# Define a null_resource to trigger the upgrade
+resource "null_resource" "upgrade_cluster" {
+  triggers = {
+    # Add a trigger to force an update when the configuration changes
+    cluster_version = your_job_cluster_resource.example_cluster.software_version
+  }
+
+  # Define the provisioner to perform the upgrade action
+  provisioner "local-exec" {
+    command = "your_upgrade_script.sh ${your_job_cluster_resource.example_cluster.cluster_name} ${your_job_cluster_resource.example_cluster.software_version}"
+  }
+}
